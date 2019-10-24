ad23a1142cd5ebef37a95bb454cd498aadaeddd3
Dan Lupascu <lupascudan1@gmail.com>
2019-10-24 22:25:10 +0300

    Display a list of auctions on the main page

--- public/index.js
+++ public/index.js
@@ -7,4 +7,34 @@ const app = feathers();
 // Connect to the server using the Socket.io connection
 app.configure(feathers.socketio(socket));
 
+const createAuctionRow = auction => {
+  return `
+    <div class="row">
+      <div class="col-12 col-md-3">
+       <img alt="placeholder" src="https://via.placeholder.com/300" class="img-thumbnail"/>
+      </div>  
+      <div class="col-12 col-md-9">
+        <h5>${auction.title}</h5>
+        <p>Starting price: $${auction.startingPrice}</p>
+        <p>Buy now price: $${auction.buyNowPrice}</p>
+      </div>
+    </div>
+  `;
+};
+
+const addAuction = auction => {
+  const auctionsContainer = document.getElementById('auctions-container');
+  auctionsContainer.innerHTML += createAuctionRow(auction);
+};
+
+const addAuctions = auctionsList => {
+  auctionsList.forEach(addAuction);
+};
+
 // All the code will go here
+const initialize = async () => {
+  const result = await app.service('auctions').find({});
+  addAuctions(result.data);
+};
+
+initialize();
